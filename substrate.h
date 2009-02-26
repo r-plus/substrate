13841a3ad13873e3764aa75a943a334e17b8c31d
Jay Freeman (saurik) <saurik@saurik.com>
2009-02-26 11:33:07 +0000

    Drop Objective-C dependency on substrate.h.

--- substrate.h
+++ substrate.h
@@ -57,7 +57,7 @@ static inline void MSHookSymbol(Type_ *&value, const char *name, void *handle) {
 
 template <typename Type_>
 static inline Type_ &MSHookIvar(id self, const char *name) {
-    Ivar ivar(class_getInstanceVariable([self class], name));
+    Ivar ivar(class_getInstanceVariable(object_getClass(self), name));
     void *pointer(ivar == NULL ? NULL : reinterpret_cast<char *>(self) + ivar_getOffset(ivar));
     return *reinterpret_cast<Type_ *>(pointer);
 }
@@ -70,6 +70,7 @@ static inline Type_ &MSHookIvar(id self, const char *name) {
 
 #define Foundation_f "/System/Library/Frameworks/Foundation.framework/Foundation"
 #define UIKit_f "/System/Library/Frameworks/UIKit.framework/UIKit"
+#define JavaScriptCore_f "/System/Library/PrivateFrameworks/JavaScriptCore.framework/JavaScriptCore"
 #define IOKit_f "/System/Library/Frameworks/IOKit.framework/IOKit"
 
 #endif//SUBSTRATE_H_
