976f4a2ee1b3e1251614309ba093ac37c2bfeeb5
Jay Freeman (saurik) <saurik@saurik.com>
2010-08-23 23:58:03 +0000

    Replace Cycript with Substrate in license.

--- package.sh
+++ package.sh
@@ -5,18 +5,18 @@
 
 # GNU Lesser General Public License, Version 3 {{{
 #
-# Cycript is free software: you can redistribute it and/or modify it under
+# Substrate is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the
 # Free Software Foundation, either version 3 of the License, or (at your
 # option) any later version.
 #
-# Cycript is distributed in the hope that it will be useful, but WITHOUT
+# Substrate is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 # License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with Cycript.  If not, see <http://www.gnu.org/licenses/>.
+# along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 # }}}
 
 arch=$1
