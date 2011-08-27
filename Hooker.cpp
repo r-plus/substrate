47e8fdad976711a148ddf9c3b15d2cb97c4bea22
Jay Freeman (saurik) <saurik@saurik.com>
2011-08-27 04:14:31 -0700

    Move x86 assembly/generation logic to x86.hpp.

--- Hooker.cpp
+++ Hooker.cpp
@@ -48,17 +48,6 @@ static _finline void MSClearCache(void *data, size_t size) {
 #endif
 }
 
-template <typename Type_>
-_disused static _finline void MSWrite(uint8_t *&buffer, Type_ value) {
-    *reinterpret_cast<Type_ *>(buffer) = value;
-    buffer += sizeof(Type_);
-}
-
-_disused static _finline void MSWrite(uint8_t *&buffer, uint8_t *data, size_t size) {
-    memcpy(buffer, data, size);
-    buffer += size;
-}
-
 #ifdef __arm__
 /* WebCore (ARM) PC-Relative:
 X    1  ldr r*,[pc,r*] !=
@@ -692,120 +681,13 @@ static void SubstrateHookFunction(SubstrateProcessRef process, void *symbol, voi
 #endif
 
 #if defined(__i386__) || defined(__x86_64__)
+
+#include "x86.hpp"
+
 static size_t MSGetInstructionWidthIntel(void *start) {
     hde64s decode;
     return hde64_disasm(start, &decode);
 }
-
-#ifdef __LP64__
-static const bool ia32 = false;
-#else
-static const bool ia32 = true;
-#endif
-
-_disused static bool MSIs32BitOffset(uintptr_t target, uintptr_t source) {
-    intptr_t offset(target - source);
-    return int32_t(offset) == offset;
-}
-
-_disused static size_t MSSizeOfSkip() {
-    return 5;
-}
-
-_disused static size_t MSSizeOfPushPointer(uintptr_t target) {
-    return uint64_t(target) >> 32 == 0 ? 5 : 13;
-}
-
-_disused static size_t MSSizeOfPushPointer(void *target) {
-    return MSSizeOfPushPointer(reinterpret_cast<uintptr_t>(target));
-}
-
-_disused static size_t MSSizeOfJump(bool blind, uintptr_t target, uintptr_t source = 0) {
-    if (ia32 || !blind && MSIs32BitOffset(target, source + 5))
-        return MSSizeOfSkip();
-    else
-        return MSSizeOfPushPointer(target) + 1;
-}
-
-_disused static size_t MSSizeOfJump(uintptr_t target, uintptr_t source) {
-    return MSSizeOfJump(false, target, source);
-}
-
-_disused static size_t MSSizeOfJump(uintptr_t target) {
-    return MSSizeOfJump(true, target);
-}
-
-_disused static size_t MSSizeOfJump(void *target, void *source) {
-    return MSSizeOfJump(reinterpret_cast<uintptr_t>(target), reinterpret_cast<uintptr_t>(source));
-}
-
-_disused static size_t MSSizeOfJump(void *target) {
-    return MSSizeOfJump(reinterpret_cast<uintptr_t>(target));
-}
-
-_disused static void MSWriteSkip(uint8_t *&current, ssize_t size) {
-    MSWrite<uint8_t>(current, 0xe9);
-    MSWrite<uint32_t>(current, size);
-}
-
-_disused static void MSPushPointer(uint8_t *&current, uintptr_t target) {
-    MSWrite<uint8_t>(current, 0x68);
-    MSWrite<uint32_t>(current, target);
-
-    if (uint32_t high = uint64_t(target) >> 32) {
-        MSWrite<uint8_t>(current, 0xc7);
-        MSWrite<uint8_t>(current, 0x44);
-        MSWrite<uint8_t>(current, 0x24);
-        MSWrite<uint8_t>(current, 0x04);
-        MSWrite<uint32_t>(current, high);
-    }
-}
-
-_disused static void MSPushPointer(uint8_t *&current, void *target) {
-    return MSPushPointer(current, reinterpret_cast<uintptr_t>(target));
-}
-
-_disused static void MSWriteJump(uint8_t *&current, uintptr_t target) {
-    uintptr_t source(reinterpret_cast<uintptr_t>(current));
-
-    if (ia32 || MSIs32BitOffset(target, source + 5))
-        MSWriteSkip(current, target - (source + 5));
-    else {
-        MSPushPointer(current, target);
-        MSWrite<uint8_t>(current, 0xc3);
-    }
-}
-
-_disused static void MSWriteJump(uint8_t *&current, void *target) {
-    return MSWriteJump(current, reinterpret_cast<uintptr_t>(target));
-}
-
-_disused static void MSWritePop(uint8_t *&current, uint8_t target) {
-    if (target >> 3 != 0)
-        MSWrite<uint8_t>(current, 0x40 | (target & 0x08) >> 3);
-    MSWrite<uint8_t>(current, 0x58 | target & 0x07);
-}
-
-_disused static size_t MSSizeOfPop(uint8_t target) {
-    return target >> 3 != 0 ? 2 : 1;
-}
-
-_disused static void MSWriteMove64(uint8_t *&current, uint8_t source, uint8_t target) {
-    MSWrite<uint8_t>(current, 0x48 | (target & 0x08) >> 3 << 2 | (source & 0x08) >> 3);
-    MSWrite<uint8_t>(current, 0x8b);
-    MSWrite<uint8_t>(current, (target & 0x07) << 3 | source & 0x07);
-}
-
-_disused static size_t MSSizeOfMove64() {
-    return 3;
-}
-
-enum I$r {
-    I$rax, I$rcx, I$rdx, I$rbx,
-    I$rsp, I$rbp, I$rsi, I$rdi,
-    I$r8, I$r9, I$r10, I$r11,
-    I$r12, I$r13, I$r14, I$r15,
-};
 
 static void SubstrateHookFunction(SubstrateProcessRef process, void *symbol, void *replace, void **result) {
     if (MSDebug)
