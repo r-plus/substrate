4898b121395593cdfe1562aba799014faa77060d
Jay Freeman (saurik) <saurik@saurik.com>
2011-11-02 15:24:29 -0700

    Improve cynject compatibility on lower firmwares.

--- Trampoline.t.cpp
+++ Trampoline.t.cpp
@@ -63,29 +63,54 @@ static void $bzero(void *data, size_t size) {
 
 
 extern "C" void Start(Baton *baton) {
+    // XXX: I am not certain if I should deallocate this port (an academic question, as I can't)
+    mach_port_t port(baton->mach_thread_self());
+
     // normally, a pthread has a _pthread associated with it; pthread_t is a pointer to it
     // these are normally initialized by either _pthread_create or _pthread_struct_init
     // however, for our purposes, just initializing it to 0 is reasonably sufficient
-    // XXX: look into using _pthread_create or _pthread_struct_init instead
+    // XXX: look into using _pthread_create instead (_pthread_struct_init is often private)
 
     struct _pthread self;
     $bzero(&self, sizeof(self));
 
 
     // this code comes from _pthread_set_self, which is the startup routine of _pthread_body
-    // XXX: on 2.x and above, __pthread_set_self doesn't even seem to do anything useful
+    // XXX: __pthread_set_self seems to syscall thread_set_cthread... what does that do?
     // XXX: if we use _pthread_create/_pthread_struct_init, the tsd[0] will be handled
 
     self.tsd[0] = &self;
     baton->__pthread_set_self(&self);
 
 
+    // on ARM, a coprocessor register has been allocated to keep track of a thread identifier
+    // on iOS << 4.2, this register points at a pthread_t. the tsd table is at +0x48 offset
+    // on iOS >= 4.2, this register points at the tsd table, with pthread_t at +0x00 offset
+
+    // here, we need to detect whether the thread register is pointing at a valid table
+    // otherwise, later code that attempts to use _pthread_setspecific_direct will crash
+    // luckily, on iOS << 4.2, we also do not need to initialize thread local storage!
+
+    bool setspecific;
+#ifdef __arm__
+    void **tsd;
+    __asm__ ("mrc p15, 0, %0, c13, c0, 3\n" : "=r"(tsd));
+    setspecific = tsd != NULL;
+#else
+    setspecific = true;
+#endif
+
+
     // the current thread identifier is stored in the 0th thread-specific data slot
     // thread-specific data, especially the 0th slot, is often stored in hardware registers
     // the _pthread_setspecific_direct macro allows us to update these static entries
-    // XXX: consider passing pthread_setspecific() and calling the slower routine
+    // we check setspecific, a variable defined above, to see whether this function works
 
-    _pthread_setspecific_direct(0, &self);
+    // note: it is not sufficient to set tsd[0] or to call _pthread_create/__pthread_set_self
+    // even more interestingly, calling the full pthread_setspecific(0) doesn't even work
+
+    if (setspecific)
+        _pthread_setspecific_direct(0, &self);
 
 
     pthread_t thread;
@@ -94,6 +119,5 @@ extern "C" void Start(Baton *baton) {
     void *status;
     baton->pthread_join(thread, &status);
 
-    // XXX: I am not certain if I should deallocate this port (an academic question, as I can't)
-    baton->thread_terminate(baton->mach_thread_self());
+    baton->thread_terminate(port);
 }
